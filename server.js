b2f2f813c607d61c2aee0e946ee5f1933d96d7b7
Feda Zidan <fido311@gmail.com>
2020-10-04 22:59:36 -0500

    Added the routes and adjust the links in html files

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+// Express configuration and sets up the basic properties for our express server
+var app = express();
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// This code to serve images, CSS files, and JavaScript files in a directory named public:
+app.use(express.static('public'));
+
+// Set up the routes to our server
+require('./routes/apiroutes')(app);
+require('./routes/htmlroutes')(app);
+
+// The below code effectively "starts" our server
+
+app.listen(PORT, function() {
+  console.log(`Server is listening on PORT: ${PORT}`);
+});
